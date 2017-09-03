f76d7b267c388f9b4b4be48fa11d227de7899f09
Rashad Kokash <richardeo112@gmail.com>
2017-09-03 16:26:35 +0300

    replaced tabs with spaces

--- src/models/Series.js
+++ src/models/Series.js
@@ -3,13 +3,13 @@ import mongoose, { Schema } from 'mongoose'
 const { ObjectId } = Schema.Types
 
 const seriesSchema = new Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-	picture: { type: String, required: true },
-	author: { type: ObjectId, ref: 'User' },
-	category: { type: String },
-	stars: { type: Number, default: 0 },
-	parts: [{ type: ObjectId }], 
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  picture: { type: String, required: true },
+  author: { type: ObjectId, ref: 'User' },
+  category: { type: String },
+  stars: { type: Number, default: 0 },
+  parts: [{ type: ObjectId }], 
 })
 
 export default mongoose.model('Series'. seriesSchema)
