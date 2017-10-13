0e6061df59e64e592058c0bdf1cdf9ed3327dc17
Rashad Kokash <richardeo112@gmail.com>
2017-10-13 17:00:08 +0300

    fix bugs and cleanup

--- src/middlewares/pagination.js
+++ src/middlewares/pagination.js
@@ -1,7 +1,7 @@
 export default (req, res, next) => {
   let { page, limit } = req.query
   page = page ? Math.max(0, parseInt(page, 10) - 1) : 0
-  limit = limit ? Math.min(50, parseInt(limit, 10)) : 10
+  limit = limit ? Math.min(50, Math.max(1, parseInt(limit, 10))) : 10
   req.pagination = {
     page,
     limit,
